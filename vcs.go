168f73a44f7e1e2af8815edc5f4203f79817d092
Satyajit Ranjeev <s@ranjeev.in>
2015-06-14 19:15:28 +0200

    Updating the vcs functionality with Checkout method.

--- vcs.go
+++ vcs.go
@@ -9,21 +9,24 @@ import (
 )
 
 type vcsCmd struct {
-	name   string
-	cmd    string
-	commit string
+	name     string
+	cmd      string
+	commit   string
+	checkout string
 }
 
 var vcsList = []*vcsCmd{
 	{
-		name:   "Git",
-		cmd:    "git",
-		commit: "git rev-parse HEAD",
+		name:     "Git",
+		cmd:      "git",
+		commit:   "git rev-parse HEAD",
+		checkout: "git checkout",
 	},
 	{
-		name:   "Mercurial",
-		cmd:    "hg",
-		commit: "hg id -i",
+		name:     "Mercurial",
+		cmd:      "hg",
+		commit:   "hg id -i",
+		checkout: "hg update",
 	},
 }
 
@@ -65,6 +68,7 @@ func vcsForDir(dir, gopath string) (vcs *vcsCmd, root string, err error) {
 // GetCommit provides the latest commit hash for a given directory.
 // If a gopath is provided it limits the search inside the path.
 func GetCommit(dir, gopath string) (string, error) {
+	dir = filepath.Join(gopath, dir)
 	vcs, root, err := vcsForDir(dir, gopath)
 	if err != nil {
 		return "", err
@@ -73,15 +77,41 @@ func GetCommit(dir, gopath string) (string, error) {
 		return "", fmt.Errorf("%s is not yet supported", vcs.name)
 	}
 	args := strings.Split(vcs.commit, " ")
-	name := args[0]
-	if len(args) > 2 {
-		args = args[1:]
+	var cmd *exec.Cmd
+	if len(args) > 1 {
+		cmd = exec.Command(args[0], args[1:]...)
+	} else {
+		cmd = exec.Command(args[0])
 	}
-	cmd := exec.Command(name, args...)
 	cmd.Dir = root
 	output, err := cmd.Output()
 	if err != nil {
 		return "", err
 	}
 	return strings.Trim(string(output), "\n"), nil
+}
+
+func Checkout(dir, gopath, hash string) error {
+	dir = filepath.Join(gopath, dir)
+	vcs, root, err := vcsForDir(dir, gopath)
+	if err != nil {
+		return err
+	}
+	if vcs.checkout == "" {
+		return fmt.Errorf("%s is not yet supported", vcs.name)
+	}
+	args := strings.Split(vcs.checkout, " ")
+	args = append(args, hash)
+	var cmd *exec.Cmd
+	if len(args) > 1 {
+		cmd = exec.Command(args[0], args[1:]...)
+	} else {
+		cmd = exec.Command(args[0])
+	}
+	cmd.Dir = root
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s\n%s", output, err)
+	}
+	return nil
 }
